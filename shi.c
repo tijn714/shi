37f62652d9843e4599bed0af07db430db3b2a959
Tijn Rodrigo <71492011+tijn714@users.noreply.github.com>
2024-02-17 16:02:05 +0100

    Update shi.c

--- shi.c
+++ shi.c
@@ -2,10 +2,23 @@
 #include <stdio.h>
 
 int main() {
-    char *data = "Hello, World!";
-    uint32_t hash;
-    shi(data);
+    char *str = "Hello, World!";
+    char *salt = "salt";
+    uint32_t *hash_one = shi(str);
+    uint32_t *hash_two = shi_salt(str, salt);
 
-    
+    for (int i = 0; i < 9; i++) {
+        printf("%x", hash_one[i]);
+    }
+
+    printf("\n");
+
+
+    for (int i = 0; i < 9; i++) {
+        printf("%x", hash_two[i]);
+    }
+
+    printf("\n");
+
     return 0;
 }
